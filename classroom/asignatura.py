29759f55b98cd7c1338e9300c1779a0480a913dd
AlPerBara <alperezba@unal.edu.co>
2024-11-23 10:17:32 -0500

    Add files via upload

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,8 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
-        self._nombre = nombre
-        self._salon = salon
+    def __init__(self, nombre, salon="remoto"): #Salida predeterminada si la clase no se hace en un salón
+        self._nombre = nombre;
+        self._salon = salon;
 
-    # def __str__(self):
-    #     pass
+    def __str__(self): #Inicialmente se remueven los comentarios
+        return f"{self._nombre} {self._salon}"; #Ahora esto va a devolver "Matemáticas remoto" et. al.
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,32 +1,34 @@
-from classroom.asignatura import Asignatura
+from classroom.asignatura import Asignatura;
 
 class Grupo:
-    grado = None
+    grado = "Grado 12"; #Este es un valor predeterminado
+                    #Aquí abajo debe decir grupo predeterminado
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+        self._grupo = grupo;
+        self._asignaturas = asignaturas if asignaturas is not None else []; #La lista se actualiza si hay estudiantes
+        self.listadoAlumnos = estudiantes if estudiantes is not None else []; #Lo de arriba
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes):
-        self._grupo = grupo
-        self._asignaturas = asignaturas
-        self.listadoAlumnos = estudiantes
-
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
+        if lista==None:
+            lista=[]
         lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos + lista
+        self.listadoAlumnos += lista
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return f"Grupo de estudiantes: {self._grupo}";
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
+    #@ classmethod
+    #def asignarNombre(cls, nombre="Grado 10"): Se quita para que el predeterminado sea el 6
+    #    cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
-        cls.grado = nombre
+    #@ classmethod
+    #def asignarNombre(cls, nombre="Grado 4"): Se quita para que el predeterminado sea el 6
+    #    cls.grado = nombre
